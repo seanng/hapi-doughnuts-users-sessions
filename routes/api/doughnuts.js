583fede1fc7d216bc9828b89b78ca74f445fe6ed
seanng <shonum@gmail.com>
2016-02-16 16:02:12 +0800

    manage to update stuff

--- routes/api/doughnuts.js
+++ routes/api/doughnuts.js
@@ -86,20 +86,42 @@ exports.register = function (server, options, next) {
       method: 'PUT',
       path: '/api/doughnuts/{id}',
       handler: function (request, reply) {
-        var db       = request.server.plugins['hapi-mongodb'].db;
-        var ObjectID = request.server.plugins['hapi-mongodb'].ObjectID;
-        var id = ObjectID(request.params.id);
-        var style = request.payload.style;
-        var flavor = request.payload.flavor;
+        Auth.authenticated(request, function(result) {
+          if (result.authenticated) {
+            var db       = request.server.plugins['hapi-mongodb'].db;
+            var ObjectID = request.server.plugins['hapi-mongodb'].ObjectID;
+            var session  = request.yar.get('hapi_doughnuts_session');
 
-        db.collection('doughnuts').findOneAndUpdate(
-          {"_id": id},
-          {style: style, flavor: flavor},
-          function (err, doc) {
-            if (err) { return reply(err); }
-            reply(doc.value);
+            var id       = ObjectID(request.params.id);
+            var user_id  = ObjectID(session.user_id);
+            var updateDoughnut = {
+              style: request.payload.style,
+              flavor: request.payload.flavor
+            };
+
+            db.collection('doughnuts').findOne({"_id": id}, function (err, doughnut) {
+              if (err) { return reply(err).code(400); }
+
+              //check if doughnut exists
+              if (doughnut === null) {
+                return reply({message: 'There is no doughnut.'}).code(404);
+              }
+
+              //if doughnut's user_id is the same as current user, then remove doughnut
+              if(doughnut.user_id.toString() === user_id.toString()) { //your doughnut
+                db.collection('doughnuts').update({"_id": id}, {$set:updateDoughnut}, function (err, doughnut) {
+                  if (err) { return reply(err).code(400); }
+                  reply(doughnut).code(200);
+                });
+              } else {
+                reply ({message: "This is not your doughnut."}).code(400);
+              }
+            });
           }
-        );
+          else {
+            reply(result).code(400);
+          }
+        })
       }
     }
   ]);
